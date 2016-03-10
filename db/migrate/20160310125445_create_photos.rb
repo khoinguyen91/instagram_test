131f7a163227f5e483b6be14a2f826aa0529c75d
jundalisay <jundalisay@yahoo.com>
2016-03-10 20:10:09 +0700

    milestone3.1

--- db/migrate/20160310125445_create_photos.rb
+++ db/migrate/20160310125445_create_photos.rb
@@ -1,12 +1,11 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-
       t.string :username
       t.text :caption
-	 # t.datetime :created_at
+      t.datetime :created_at
       t.integer :likes_count
-	  t.string :url
+      t.string :url
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310122806) do
+ActiveRecord::Schema.define(version: 20160310125445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 20160310122806) do
   create_table "photos", force: :cascade do |t|
     t.string   "username"
     t.text     "caption"
+    t.datetime "created_at",  null: false
     t.integer  "likes_count"
     t.string   "url"
-    t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
